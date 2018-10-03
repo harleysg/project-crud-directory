cf47ac34770036d412c61adfed5b0daa13b83d9d
Harley Santos Garzón <santo.gh@gmail.com>
2018-10-03 16:06:33 -0500

    update: optimización de consulta

--- data/includes/sql/usuarios/listar_usuarios.php
+++ data/includes/sql/usuarios/listar_usuarios.php
@@ -1,15 +1,21 @@
 <?php
-include("../../class/conexion.php");
+    include("../../class/conexion.php");
 	$post_variables = file_get_contents("php://input");
     $variable = json_decode($post_variables);
+    $id_user = $variable->id_user;
+    //$_POST['id_user'];
 
     if(isset($variable->control) && $variable->control == "SI"){
 
         $rowusuarios = array();
 
-        $selec_usuarios="SELECT * FROM directorio";
+        if($variable->query == "all"){
+            $selec_usuarios="SELECT id, name, last_nam, tel, img_ruta, img_nam, img_form FROM `directorio`";
+        }
+        if($variable->query == "person"){
+            $selec_usuarios="SELECT id, name, last_nam, tel, img_ruta, img_nam, img_form FROM `directorio` WHERE id = $id_user";
+        }
         $result = mysql_query($selec_usuarios);
-
 
         while ($row_usuarios = mysql_fetch_assoc($result)) {
             $rowusuarios[] = $row_usuarios;
